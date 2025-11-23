239328ccc23eef5c0c108339c3a6a2195e0978ec
matthew6-ops <matthew.helfer6@gmail.com>
2025-11-23 15:33:51 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html>
       <body>{children}</body>
     </html>
-  );
+  )
 }
