aee91d49fc6b80d0f31c5a23be3ccea7b8496df4
matthew6-ops <matthew.helfer6@gmail.com>
2025-11-24 12:10:32 -0600

    f

--- .next/dev/server/chunks/ssr/[root-of-the-server]__f1627132._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__f1627132._.js
@@ -57,14 +57,6 @@ function HomePage() {
                     fileName: "[project]/app/page.tsx",
                     lineNumber: 17,
                     columnNumber: 9
-                }, this),
-                /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
-                    className: "text-xs text-slate-500",
-                    children: "Paper bankroll only. If you ruin your life, that's on you."
-                }, void 0, false, {
-                    fileName: "[project]/app/page.tsx",
-                    lineNumber: 24,
-                    columnNumber: 9
                 }, this)
             ]
         }, void 0, true, {
